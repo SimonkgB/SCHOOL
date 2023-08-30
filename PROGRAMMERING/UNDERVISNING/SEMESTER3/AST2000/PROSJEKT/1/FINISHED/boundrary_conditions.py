670d42843f70cc515ecd23c47587587f43cebf8f
SimonkgB <131837414+SimonkgB@users.noreply.github.com>
2023-08-30 23:55:05 +0200

    ahh yes no velc

--- PROGRAMMERING/UNDERVISNING/SEMESTER3/AST2000/PROSJEKT/1/FINISHED/boundrary_conditions.py
+++ PROGRAMMERING/UNDERVISNING/SEMESTER3/AST2000/PROSJEKT/1/FINISHED/boundrary_conditions.py
@@ -8,13 +8,21 @@ import consts as cs
 
 escaped_velocities = []
 
+
+def velocity_distribution(temperature):
+    sigma = np.sqrt((cs.boltzmann_constant * temperature) / (cs.hydrogen2_mass))
+    return np.random.normal(0, sigma, (cs.num_particles, 3))
+
+velocity = velocity_distribution(cs.temperature)
+
+
 def boundary_conditions(position, velocity, box_length, hole_radius, hole_center):  # Apply boundary conditions and check for particles falling into the hole
     for i, pos in enumerate(position):   # Check for particles falling into the hole
         distance_to_center =np.linalg.norm(pos[:2] - hole_center)
         if distance_to_center < hole_radius and pos[2] <= 0: # Reset the position and velocity of the particle
             escaped_velocities.append(velocity[i].copy())  #storing velocities of particles escaping
             position[i] = np.array([box_length/2, box_length/2, box_length])    # sett particle to "spawn" in the center of x-y plane and top of z-plane
-            velocity[i] =velc.setting_velocity(velc.velocity_distribution(cs.hydrogen2_mass))
+            velocity[i] = np.random.normal(0, np.sqrt((cs.boltzmann_constant * cs.temperature) / (cs.hydrogen2_mass)),3)
 
     index =np.where((position >box_length) | (position < 0)) # Reflective boundary conditions
     velocity[index] *=-1
---
+++ PROGRAMMERING/UNDERVISNING/SEMESTER3/AST2000/PROSJEKT/1/FINISHED/rocket_calculations.py
@@ -0,0 +1,38 @@
+import numpy as np
+import scipy.constants as scp_c
+
+import sys
+sys.path.append("C:/Users/simon/Dokumenter/SCHOOL/PROGRAMMERING/UNDERVISNING/SEMESTER3/AST2000/PROSJEKT/1")  # Add the directory containing the script
+import goated as w
+import boundrary_conditions as b_c
+import velocities as velc
+import consts as cs
+
+
+momentum_particles = np.sum(w.escaped_velocities*cs.hydrogen2_mass, axis=0)  # indicates that we are suming the x-components so we dont need a for loop
+momentum_rocket = -momentum_particles
+v_r = momentum_rocket / cs.rocket_mass
+
+
+# function for calculating total force
+def calculate_force(v_r):
+    # Calculate the change in momentum
+    
+    # Calculate the force using F = Δp/Δt
+    force_x = v_r[0]/cs.time_step
+    force_y = v_r[1]/cs.time_step
+    force_z = v_r[2]/cs.time_step
+    
+    return np.array([force_x, force_y, force_z])
+
+def massflow():
+    return cs.hydrogen2_mass*w.escaped_velocities.shape[0]/cs.totaltime
+
+fz = calculate_force(v_r)[2]
+
+"""
+print(f"summen av partikkelenes momentum: {momentum_particles}")
+print (f"rockets momentum: {momentum_rocket}")
+print(f"Rocket Velocity: {v_r[2]}")
+print(f"kraft done by particles: {calculate_force(v_r)[2]}")
+"""
